6ff7788d6a3788c2d2f10692dfea91d67b37677a
DanielYeung0131 <daniel.yeung.0131@gmail.com>
2025-06-18 02:12:15 -0500

    unsecure pw

--- lib/db.ts
+++ lib/db.ts
@@ -1,12 +1,18 @@
 // lib/db.ts
 import mysql from "mysql2/promise";
 
+// DB_HOST=store-reservation-system.cfa0is4ckgb5.us-east-2.rds.amazonaws.com
+// DB_PORT=3306
+// DB_USER=admin
+// DB_PASSWORD=Daniel0131!
+// DB_NAME=reservation_db
+
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  port: parseInt(process.env.DB_PORT || "3306"),
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: "store-reservation-system.cfa0is4ckgb5.us-east-2.rds.amazonaws.com",
+  port: 3306,
+  user: "admin",
+  password: "Daniel0131!",
+  database: "reservation_db",
 });
 
 export default pool;
