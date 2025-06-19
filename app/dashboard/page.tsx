2ee6ecfe0b4a8302e96700f39edecd0b283ef104
DanielYeung0131 <daniel.yeung.0131@gmail.com>
2025-06-19 00:28:22 -0500

    A

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -38,14 +38,9 @@ const createAppointment = async (appointment: Appointment) => {
     // const timezoneOffsetMinutes = new Date().getTimezoneOffset();
     // const timezoneOffsetMs = timezoneOffsetMinutes * 60 * 1000 * -1;
 
-    const prevDayStart = new Date(
-      new Date(appointment.start).getTime() -
-        TexasTimeZoneOffset * 60 * 60 * 1000
-    );
-    const prevDayEnd = new Date(
-      new Date(appointment.end).getTime() - TexasTimeZoneOffset * 60 * 60 * 1000
-    );
-    appointment = { ...appointment, start: prevDayStart, end: prevDayEnd };
+    // const prevDayStart = new Date(new Date(appointment.start).getTime());
+    // const prevDayEnd = new Date(new Date(appointment.end).getTime());
+    // appointment = { ...appointment, start: prevDayStart, end: prevDayEnd };
     console.log("HERE creating appointment:", appointment);
     const response = await fetch("/api/appointments", {
       method: "POST",
