60db016cb2e23fbcb51dab7a9525516ebc853891
DanielYeung0131 <daniel.yeung.0131@gmail.com>
2025-06-18 22:57:39 -0500

    fix env

--- lib/db.ts
+++ lib/db.ts
@@ -1,18 +1,12 @@
 // lib/db.ts
 import mysql from "mysql2/promise";
 
-// DB_HOST=store-reservation-system.cfa0is4ckgb5.us-east-2.rds.amazonaws.com
-// DB_PORT=3306
-// DB_USER=admin
-// DB_PASSWORD=Daniel0131!
-// DB_NAME=reservation_db
-
 const pool = mysql.createPool({
-  host: "store-reservation-system.cfa0is4ckgb5.us-east-2.rds.amazonaws.com",
-  port: 3306,
-  user: "admin",
-  password: "Daniel0131!",
-  database: "reservation_db",
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT ? Number(process.env.DB_PORT) : undefined,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 export default pool;
