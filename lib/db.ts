666aa464fda40de546229f60dc0a8a1d86fae651
DanielYeung0131 <daniel.yeung.0131@gmail.com>
2025-06-18 23:19:29 -0500

    NEXT_PUBLIC_

--- lib/db.ts
+++ lib/db.ts
@@ -2,11 +2,13 @@
 import mysql from "mysql2/promise";
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT ? Number(process.env.DB_PORT) : undefined,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: process.env.NEXT_PUBLIC_DB_HOST,
+  port: process.env.NEXT_PUBLIC_DB_PORT
+    ? Number(process.env.NEXT_PUBLIC_DB_PORT)
+    : undefined,
+  user: process.env.NEXT_PUBLIC_DB_USER,
+  password: process.env.NEXT_PUBLIC_DB_PASSWORD,
+  database: process.env.NEXT_PUBLIC_DB_NAME,
 });
 
 export default pool;
